82bf23847aa3fd777751822d4d2c26e3937b72f7
kashifest <kashif.khan@est.tech>
2020-06-24 12:15:55 +0300

    Add ServerAliveCountMax for ssh connection

--- jenkins/scripts/integration_test.sh
+++ jenkins/scripts/integration_test.sh
@@ -92,6 +92,7 @@ ssh \
   -o StrictHostKeyChecking=no \
   -o UserKnownHostsFile=/dev/null \
   -o ServerAliveInterval=15 \
+  -o ServerAliveCountMax=10 \
   -i "${AIRSHIP_CI_USER_KEY}" \
   "${AIRSHIP_CI_USER}"@"${TEST_EXECUTER_IP}" \
   PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin \
