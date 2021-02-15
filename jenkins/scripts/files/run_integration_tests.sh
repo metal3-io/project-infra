7a00ec72f4856fed7cbd6997b68518c34f78cd72
smoshiur1237 <moshiur.rahman@est.tech>
2021-02-15 09:32:47 +0200

    Enable basic authentication and TLS for upgrade CI job.

--- jenkins/scripts/files/run_integration_tests.sh
+++ jenkins/scripts/files/run_integration_tests.sh
@@ -130,8 +130,6 @@ fi
 
 if [[ "${TESTS_FOR}" == "feature_tests_upgrade"* ]]
 then
-  export IRONIC_BASIC_AUTH="false"
-  export IRONIC_TLS_SETUP="false"
   export NODE_DRAIN_TIMEOUT="300s"
   make "${TESTS_FOR}"
 elif [[ "${TESTS_FOR}" == "feature_tests" || "${TESTS_FOR}" == "feature_tests_centos" ]]
