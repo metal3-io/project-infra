29f18e2f08a5715969ecefc98a52e899acbaea60
Michael Captain <michael.captain@est.tech>
2021-10-19 12:16:31 +0300

    Fix jobs after change to Fra1 region
    The previous pattern matching logic for UPDATED_BRANCH being main or
    master doesn't work in bash (only in zsh). This is updated so that the
    OpenStack environment variables are set correctly now if we're running
    the job on a main branch.
    
    Furthermore, logging is added so that the region is echoed in all runs.
    
    Additionally, the cleanup job was broken by the previous change. It is
    now updated to run resource cleanup in both regions.

--- jenkins/scripts/integration_delete.sh
+++ jenkins/scripts/integration_delete.sh
@@ -19,11 +19,14 @@ source "${CI_DIR}/utils.sh"
 
 TEST_EXECUTER_PORT_NAME="${TEST_EXECUTER_PORT_NAME:-${TEST_EXECUTER_VM_NAME}-int-port}"
 
-if [[ "${UPDATED_BRANCH}" =~ "^(main|master)$" ]]
+# Run feature tests and main and master integration tests in the Frankfurt region
+if [[ "${TESTS_FOR}" == "feature_tests"* ]] || \
+   [[ "${UPDATED_BRANCH}" == "main" ]] || [[ "${UPDATED_BRANCH}" == "master" ]]
 then
   OS_REGION_NAME="Fra1"
   OS_AUTH_URL="https://fra1.citycloud.com:5000"
 fi
+echo "Running in region: $OS_REGION_NAME"
 
 # Delete executer vm
 echo "Deleting executer VM ${TEST_EXECUTER_VM_NAME}."
