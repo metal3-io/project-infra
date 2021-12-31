4653a83afba7551720e13374072e487714f70712
Lennart Jern <lennart.jern@est.tech>
2021-12-31 13:04:37 +0200

    Create/delete floating IP in Fra1
    This is to avoid collisions when multiple jobs would happen to select
    the same IP. If each job creates its own IP we get no collisions.

--- jenkins/scripts/integration_delete.sh
+++ jenkins/scripts/integration_delete.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # Description:
-# Runs in every single main job and in jobs triggered within the PR in metal3 repos. 
+# Runs in every single main job and in jobs triggered within the PR in metal3 repos.
 # Consumed by integration_tests.pipeline and cleans the executer vm/volume and port after
 # integration tests.
 #   Requires:
@@ -18,6 +18,7 @@ CI_DIR="$(dirname "$(readlink -f "${0}")")"
 source "${CI_DIR}/utils.sh"
 
 TEST_EXECUTER_PORT_NAME="${TEST_EXECUTER_PORT_NAME:-${TEST_EXECUTER_VM_NAME}-int-port}"
+TEST_EXECUTER_FIP_TAG="${TEST_EXECUTER_FIP_TAG:-${TEST_EXECUTER_VM_NAME}-floating-ip}"
 
 # Run feature tests, e2e tests, main, master and release* tests in the Frankfurt region
 if [[ "${TESTS_FOR}" == "feature_tests"* ]] || [[ "${TESTS_FOR}" == "e2e_tests"* ]] || \
@@ -27,6 +28,17 @@ then
   OS_AUTH_URL="https://fra1.citycloud.com:5000"
 fi
 echo "Running in region: $OS_REGION_NAME"
+
+if [[ "$OS_REGION_NAME" != "Kna1" ]]
+then
+  # Find executer floating ip
+  TEST_EXECUTER_FIP_ID="$(openstack floating ip list --tags "${TEST_EXECUTER_FIP_TAG}" -f value -c ID)"
+
+  # Delete executer floating ip
+  echo "Deleting executer floating IP ${TEST_EXECUTER_FIP_ID}."
+  echo "${TEST_EXECUTER_FIP_ID}" | xargs openstack floating ip delete
+  echo "Executer floating IP ${TEST_EXECUTER_FIP_ID} is deleted."
+fi
 
 # Delete executer vm
 echo "Deleting executer VM ${TEST_EXECUTER_VM_NAME}."
