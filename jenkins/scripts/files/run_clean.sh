de7583d3fc248d32d3751f0481a68f917ddb6776
Rozzii <adam.rozman@est.tech>
2022-08-01 14:40:43 +0300

    fix incorrect metal3 dev env path
    The cleanup script worked with the wron metal3 dev env path.

--- jenkins/scripts/files/run_clean.sh
+++ jenkins/scripts/files/run_clean.sh
@@ -6,6 +6,9 @@ REPO_NAME="${1:-metal3-dev-env}"
 IMAGE_OS="${2:-ubuntu}"
 TESTS_FOR="${3:-e2e_tests}"
 
+# shellcheck disable=SC1090
+source "/tmp/vars.sh"
+
 if [ "${IMAGE_OS}" == "ubuntu" ]; then
   export CONTAINER_RUNTIME="docker"
   export EPHEMERAL_CLUSTER="kind"
@@ -13,7 +16,7 @@ else
   export EPHEMERAL_CLUSTER="minikube"
 fi
 if [[ ("${TESTS_FOR}" != "e2e_tests" && "${REPO_NAME}" == "metal3-dev-env") ||
-      ("${TESTS_FOR}" == "e2e_tests" && "${REPO_NAME}" == "cluster-api-provider-metal3") 
+      ("${TESTS_FOR}" == "e2e_tests" && "${REPO_NAME}" == "cluster-api-provider-metal3")
   ]]; then
   pushd tested_repo
 else
