f5ca9da15f9525f25f53386c93717cc9f24b468a
Kashif Khan <kashif.khan@est.tech>
2025-02-07 10:07:38 +0200

    Bump k8s to v1.32.1 and CRIO to v1.32.1
    Bumping k8s and CRIO to latest patch version for v1.32.x
    Signed-off-by: Kashif Khan <kashif.khan@est.tech>

--- jenkins/image_building/build-image.sh
+++ jenkins/image_building/build-image.sh
@@ -49,8 +49,8 @@ fi
 if [[ "${IMAGE_TYPE}" == "node" ]]; then
   # The default data source for cloud-init element is exclusively Amazon EC2
   export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive"
-  export KUBERNETES_VERSION="${KUBERNETES_VERSION:-"v1.32.0"}"
-  export CRIO_VERSION="${CRIO_VERSION:-"v1.31.3"}"
+  export KUBERNETES_VERSION="${KUBERNETES_VERSION:-"v1.32.1"}"
+  export CRIO_VERSION="${CRIO_VERSION:-"v1.32.1"}"
   export CRICTL_VERSION="${CRICTL_VERSION-"v1.32.0"}"
   img_name="${IMAGE_OS^^}_${numeric_release}_NODE_IMAGE_K8S_${KUBERNETES_VERSION}"
 else
