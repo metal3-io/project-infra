9fa66ca84bebe9ed096a14822281027754144ced
Sunnatillo <sunnat.samadov@est.tech>
2025-12-08 10:21:26 +0200

    Add new test scripts for bml
    Signed-off-by: Sunnatillo <sunnat.samadov@est.tech>

---
+++ jenkins/scripts/bare_metal_lab/bml_test/clean.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x
+
+sudo ip link delete external
+sudo ip link delete ironic-peer
+sudo ip link delete ironicendpoint
+sudo ip link delete provisioning
+
+# Destroy and undefine the libvirt networks
+sudo virsh net-destroy provisioning
+sudo virsh net-destroy external
+
+sudo virsh net-undefine provisioning
+sudo virsh net-undefine external
+
+
+minikube delete
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+# sudo rm -rf /opt/metal3-dev-env/ironic/*
+sudo rm -rf  /home/metal3ci/go/src/github.com/metal3-io/*
